82bda8beb7224aa533b9319cd780d21b9265d197
James Min <jmin499@gmail.com>
2019-10-30 02:58:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   
   
   def add_item(title, price, quantity = 1)
-    self.total += (price * quantity)
+    @total += (price * quantity)
     # h = {:title => title, :price => price, :quantity => quantity}
   end
   
