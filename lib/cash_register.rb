def6791ed41cf56e1bfb47ba9d67216b05b39cfd
James Min <jmin499@gmail.com>
2019-10-30 02:49:53 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+require "pry"
 
+class CashRegister
+  
+  attr_accessor :total
+  attr_reader :discount, :title, :price, :quantity
+  
+  def initialize(total = 0)
+    @total = total
+  end
+  
+  def discount
+    @discount = true
+    @total
+  end
+  
+  def add_item(title, price, quantity = 1)
+    self.total += (price * quantity)
+    h = {:title => title, :price => price, :quantity => quantity}
+  end
+  
+  def apply_discount
+    self.total
+    # self.total -= (self.total * 0.2)
+  end
+  
+end
