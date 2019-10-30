342645c6485f6bbffdb7cc5e516f58bece98a65c
James Min <jmin499@gmail.com>
2019-10-30 03:14:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,17 +10,13 @@ class CashRegister
     @total = 0
   end
   
-  # def total=(total_price)
-  #   @total += total_price
-  # end
-  
   def add_item(title, price, quantity = 1)
     self.total += (price * quantity)
   end
   
-  # def apply_discount
-  #   discount = self.discount * 0.01
-  #   self.total -= (self.total * discount)
-  # end
+  def apply_discount
+    discount = self.discount * 0.01
+    self.total -= (self.total * discount)
+  end
   
 end
